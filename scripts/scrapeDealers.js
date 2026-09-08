94ce9688169bc5da0286106f5aee22d96a3062ec
ryansaballa <154952941+ryansaballa@users.noreply.github.com>
2026-09-08 16:36:23 -0400

    Update scrapeDealers.js

--- scripts/scrapeDealers.js
+++ scripts/scrapeDealers.js
@@ -3,3 +3,14 @@ const cheerio = require ("cheerio");
 const fs = require ("fs");
 
 console.log("Start Scraping");
+
+async function scrapePage(page) {
+  const url =
+    `https://www.ciro.ca/office-investor/dealers-we-regulate?page=${page}`;
+
+  const response = await axios.get(url);
+
+  console.log(`Downloaded page ${page}`);
+
+  return response.data;
+}
